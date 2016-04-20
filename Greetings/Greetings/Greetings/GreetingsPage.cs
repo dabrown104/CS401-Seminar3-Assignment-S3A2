5be6a24c0bd9c5cd0d4c1406224e09ff04105bc4
dabrown104 <raven122685@yahoo.com>
2016-04-20 16:40:49 -0400

    Assignment S3A2

---
+++ Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public partial class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!",
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center
+            };
+
+            Device.OnPlatform(iOS: () =>
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            });
+        }
+    }
+}
---
+++ HelloSap/HelloSap/HelloSap/App.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HelloSap
+{
+	public class App : Application
+	{
+		public App ()
+		{
+
+
+        #if __IOS__
+            // iOS specific code
+        #elif __ANDROID__
+            // Android specific code
+        #elif WINDOWS_UWP
+            // Universal Windows Platform specific code
+        #elif WINDOWS_APP
+            // Windows 8.1 specific code
+        #elif WINDOWS__PHONE_APP
+            // Windows Phone 8.1 specific code
+        #endif
+
+        }
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
